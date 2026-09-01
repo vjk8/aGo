e39cd8ccf79a1fb0716848544010195f12148b2e
Vijay <vijay@krishnamoorthy.com>
2026-08-31 23:31:08 -0400

    setup classes

---
+++ aGo/vae.py
@@ -0,0 +1,10 @@
+class Encoder(nn.Module):
+    def __call__():
+        pass
+
+def reparameterize(rng, mu, logvar):
+    return None
+
+class Decoder(nn.Module):
+    def __call__():
+        pass
